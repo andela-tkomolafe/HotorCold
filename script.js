7cb91bf174adcdda9698d7117f10eef5eac5a37a
Tolu Komolafe <betkomuniq@yahoo.com>
2014-09-05 18:47:34 +0100

    CSS folder and image included

--- script.js
+++ script.js
@@ -1,81 +1,114 @@
-$(document).ready();
-var prevNumber = 0;
-var userChoice;
+var prevNumber = 'Nil';
 var diff1 = 0;
 var diff2 = 0;
-/****************************
+
+/**************************
 	GENERATE RANDOM NUMBER
-******************************/
+***************************/
 var compChoice = Math.floor(Math.random() * 100);
 console.log(compChoice);
-var pickRandom = function(){
-	return compChoice;
+
+var userChoice;
+
+
+/**********************************
+	CHECK VALIDITY OF USER CHOICE
+***********************************/
+isNumberValid = function(){
+	// var userChoice = parseInt($('#userChoice').val());
+	if( isNaN(userChoice) || userChoice === null || userChoice === undefined || userChoice > 100){
+		console.log('not a no.');
+		$('.feedback1').text("You must enter a valid number between 0 and 100");
+	}
+	else{
+		compareChoices();
+	}
+
 };
-getUserChoice = function(){
-	userChoice = parseInt($('#userChoice').val());
-};
+
 /********************************
-	CHECK IF USERCHOICE IS VALID
-**********************************/
-function isNumberValid(){
+	COMPARE THE CHOICES
+*********************************/
+compareChoices = function(){
+		console.log('Inside compareChoices');
+		if(prevNumber === 'Nil'){
 
-		while(isNaN(userChoice) || (userChoice === null) || (userChoice === undefined) || (userChoice > 100) || (userChoice < 0)){
-			alert("You have to enter a valid number between 0 and 100");
-			getUserChoice();
-		}
-	}; 
-
-function compareChoice(){
-	
-		if (userChoice < compChoice){
-		alert("Cold");
-			while(userChoice !== compChoice){
-				prevNumber = userChoice;
-				checkAgain();
-				break;
-				console.log('while loop');
-			}
-		}
-		else if(userChoice > compChoice){
-		alert("Hot");
-			while(userChoice !== compChoice){
-				prevNumber = userChoice;
-				checkAgain();
-				break;
-
-			}
-		}
-		else if(userChoice === compChoice){
-		alert("You guessed right!")
+			if(userChoice === compChoice){
+				$('.feedback1').text("You guessed right!!!");
 			
+			}
+	
+			else if(userChoice < compChoice){
+				$('.feedback1').text("You are Cold!");
+				 prevNumber = userChoice;
+			 
+			
+			}
+			else if(userChoice > compChoice){
+			$('.feedback1').text("You are Hot!");
+				 prevNumber = userChoice
+			
+			}
 		}
-};
-function check(){
-	getUserChoice();	
-	isNumberValid();
-	compareChoice();
+		else {
+			
+			diff1 = Math.abs(prevNumber - compChoice);
+			diff2 = Math.abs(userChoice - compChoice);
+			if(userChoice === compChoice){
+				$('.feedback1').text("You guessed right!!!");
+
+			}
+			else if(diff1 > diff2){
+				$('.feedback1').text("You are Hotter!");
+				prevNumber = userChoice;
+			}
+			else if(diff1 < diff2){
+				$('.feedback1').text("You are Colder!");
+				prevNumber = userChoice;
+			}
+
+		}
+	
+		
 };
 
-function checkAgain(){
-getUserChoice();
-isNumberValid();
-diff1 = Math.abs(prevNumber - compChoice);
-diff2 = Math.abs(userChoice - compChoice);
-if(diff1 > diff2){
-	alert ("Hotter!")
-}
-else if(diff1 < diff2){
-	alert("colder")
-}
 
-};
+// check = function(){
+// 	compareChoices();
+
+// };
+// checkAgain = function(){
+// 	isNumberValid();
+// 	diff1 = Math.abs(prevNumber - compChoice);
+// 	diff2 = Math.abs(userChoice - compChoice);
+// 	if(userChoice === compChoice){
+// 		$('.feedback1').text("You guessed right!!!");
+
+// 	}
+// 	else if(diff1 > diff2){
+// 		$('.feedback1').text("You are Hotter!");
+// 		prevNumber = userChoice;
+// 	}
+// 	else if(diff1 < diff2){
+// 		$('.feedback1').text("You are Colder!");
+// 		prevNumber = userChoice;
+// 	}
+
+// };
+
+
 
 $('#check').click(function(event){
-	console.log('youre');
 	event.preventDefault();
-	getUserChoice();	
+	userChoice = parseInt($('#userChoice').val());
+	console.log(typeof userChoice);
 	isNumberValid();
-	compareChoice();
 });
 
 
+// $('#checkAgain').click(function(event){
+// 	event.preventDefault();
+// 	userChoice = parseInt($('#userChoice').val());
+// 	console.log(typeof userChoice);
+// 	checkAgain();
+// });
